b9a17a7cfc10523b895df02f17c81e0306b6ed92
Emmanuel <emmanuel.maurice@gmail.com>
2021-03-03 12:56:24 +0100

    first commit

---
+++ src/app/formulaire/formulaire.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire',
+  templateUrl: './formulaire.component.html',
+  styleUrls: ['./formulaire.component.css']
+})
+export class FormulaireComponent implements OnInit {
+
+  valid1 : boolean = true;
+  valid2 : boolean = false;
+  prenom : string = "toto";
+  
+  click : string = "";
+  constructor() { }
+
+  listArticles  = ["livre","ordinateur","stylo","verre"];
+
+  ngOnInit(): void {
+  }
+
+  click1 () {
+    console.log ("click");
+    this.click = "click1";
+    this.valid1 = false;
+    this.valid2 = true;
+
+  }
+
+  click2 () {
+    console.log ("click");
+    this.click = "click2";
+    this.valid1 = true;
+    this.valid2 = false;
+
+  }
+  
+
+}
